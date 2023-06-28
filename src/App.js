0faa1f4c244f4ab147e081cab4f8e4d22d676922
George Chen <ghchen10@outlook.com>
2023-06-28 18:26:54 +0100

    Added Contact Us Page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage/HomePage';
 import Signup from './pages/SignupPage/Signup';
+import ContactUs from './pages/ContactUsPage/ContactUs';
 import GlobalStyle from './globalStyles';
 import Navbar from './components/Navbar/Navbar';
 import Footer from './components/Footer/Footer';
@@ -14,6 +15,7 @@ function App() {
 			<Switch>
 				<Route path="/" exact component={HomePage} />
 				<Route path="/sign-up" exact component={Signup} />
+				<Route path="/contact-us" exact component={ContactUs} /> 
 			</Switch>
 			<Footer />
 		</Router>
